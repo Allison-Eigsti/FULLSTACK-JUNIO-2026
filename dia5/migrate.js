7eb287ea4b1d4829a57a0b58e6c343db2a8d1aa7
Allison-Eigsti <allisonmeigsti@gmail.com>
2026-07-06 18:57:03 +0200

    Migrate csv

--- dia5/migrate.js
+++ dia5/migrate.js
@@ -1,20 +1,28 @@
+// Migrate users from a CSV file to the database
 const csv = require('csv-parser')
 const fs = require('fs')
+const connectDB = require('./db.js')
 const User = require('./models/User.js')
 const results = [];
 
-fs.createReadStream('data.csv')
-  .pipe(csv( { separator: ';' } ));
+connectDB();
+
+
+// Script to migrate
+fs.createReadStream('users.csv')
+  .pipe(csv( { separator: ';' } ))
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    results.forEach(async (user) => {
-        try {
-            const { name, email, age } = user;
-            const newUser = new User({ name, email, age });
-            await newUser.save();
-            console.log(`User ${name} saved successfully.`);
-        } catch (error) {
-            console.error(`Error saving user ${user.name}:`, error.message);
-        }
-  });
-)
+    results.forEach(async (userData) => {
+        console.log(userData);
+        const { name, email, age } = userData;
+        const newUser = new User({ name, email, age });
+            try {
+                await newUser.save();
+                console.log(`User ${name} saved successfully.`);
+            } catch (error) {
+                console.error(`Error saving user ${name}:`, error.message);
+            }
+        });
+        
+    });
