8f6d9259e8ca7fe259e90c55d24e23f30c83e223
AllisonE-code <allisonmeigsti@gmail.com>
2026-07-08 21:34:43 +0200

    Wrote API

--- dia6/config/db.js
+++ dia6/config/db.js
@@ -2,3 +2,14 @@ const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/dia6'
 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI)
+        console.log('Database connected successfully')
+    } catch (error) {
+        console.error(`Error connecting to Mongo`, error.message)
+        process.exit(1)
+    } 
+}
+
+module.exports = connectDB;
---
+++ dia6/controller/product-controllers.js
@@ -0,0 +1,65 @@
+const Product = require('../models/Product.js')
+
+function getProducts(req, res) {
+    Product.find()
+        .then(products => res.json(products))
+        .catch(err => res.status(500).json({ message: err.message }));
+}
+
+function getProductById(req, res) {
+    Product.findById(req.params.id)
+        .then(product => {
+            if (!product) {
+                return res.status(404).json({ message: "Could not find product" })
+            }
+            res.json(product);
+        })
+        .catch(err => res.status(500).json({ message: err.message }))
+}
+
+
+// Figure out a better way to return
+function addProduct(req, res) {
+    const { name, price, quantity_left } = req.body;
+    const newProduct = new Product({ name, price, quantity_left });
+    newProduct.save()
+        .then(product => res.send(201).json(product))
+        .catch(err => res.status(400).json({ message: err.message }))
+}
+
+function updateProduct(req, res) {
+    const { name, price, quantity_left } = req.body;
+    Product.findByIdAndUpdate(
+        req.params.id,
+        { name, price, quantity_left },
+        { new: true }
+    )
+        .then(updateProduct => {
+            if (!updateProduct) {
+                return res.status(404).json({ message: "Product not found"})
+            }
+            res.json(updatedProduct);
+        })
+        .catch(err => res.status(400).json({ message: err.message }))
+}
+
+function deleteProduct(req, res) {
+    Product.findByIdAndDelete(req.params.id)
+        .then(deletedProduct => {
+            if (!deletedProduct) {
+                return res.status(404).json({ message: "Product not found" })
+            }
+            res.json({ message: "Product deleted successfully "})
+        })
+        .catch(err => res.status(500).json({ message: err.message }));
+}
+
+
+
+module.exports = {
+    getProducts,
+    getProductById,
+    addProduct, 
+    updateProduct,
+    deleteProduct
+}
