2e61e96a14eca2ac74b051ee7fb19183f0e9cf1a
AllisonE-code <allisonmeigsti@gmail.com>
2026-07-08 08:46:48 +0200

    Added index.js to dia6 and db.js

--- dia5/db.js
+++ dia5/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 // Defining database connection 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/dia5'
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/dia5'
 
 // Start up database connection (async function w a try-catch block)
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI)
+        await mongoose.connect(MONGODB_URL)
         console.log('Connected to MongoDB'); 
     } catch (error) {
         console.error(`Error connecting to MongoDB`, error.message);
---
+++ dia6/config/db.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/dia6'
+
